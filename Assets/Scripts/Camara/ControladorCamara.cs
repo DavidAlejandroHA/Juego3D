e722f0bdfe4dd05b57fef28e26d3fe6e3f1a0740
David Alejandro <99730787+DavidAlejandroHA@users.noreply.github.com>
2023-10-24 17:09:16 +0200

    Trabajando la cámara

--- Assets/Scripts/Camara/ControladorCamara.cs
+++ Assets/Scripts/Camara/ControladorCamara.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class ControladorCamara : MonoBehaviour
 {
-    public GameObject cubo;
+    //public GameObject cubo;
+    public Transform personaje;
     public float offsetZ, offsetY;
     float auxoffsetZ, auxoffsetY;
     bool primeraPersona = false;
+    float sensibilidad = 2f;
+    float rotacionCamaraVertical = 0f;
+    float rotacionCamaraHorizontal = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +27,40 @@ public class ControladorCamara : MonoBehaviour
             primeraPersona = !primeraPersona;
         }
 
-        transform.position = new Vector3(cubo.transform.position.x, 
-            cubo.transform.position.y - (primeraPersona ? 0 : offsetY), 
+        //transform.position = new Vector3(cubo.transform.position.x,
+        //cubo.transform.position.y - (primeraPersona ? 0 : offsetY),
+        //cubo.transform.position.z - (primeraPersona ? 0 : offsetZ));
+
+
+        // 
+
+
+        transform.position = new Vector3(cubo.transform.position.x,
+            cubo.transform.position.y - (primeraPersona ? 0 : offsetY),
             cubo.transform.position.z - (primeraPersona ? 0 : offsetZ));
 
-        
+        // Si se está en primera persona se trabaja las rotaciones de la cámara
+        if (primeraPersona)
+        {
+
+            // Recolección de los movimientos del ratón
+            float inputX = Input.GetAxis("Mouse X") * sensibilidad;
+            float inputY = Input.GetAxis("Mouse Y") * sensibilidad;
+
+            // Rota la cámara al rededor del eje Y
+            rotacionCamaraVertical -= inputY;
+            rotacionCamaraHorizontal -= inputX;
+            rotacionCamaraVertical = Mathf.Clamp(rotacionCamaraVertical, -90f, 90f);
+            transform.localEulerAngles = (Vector3.right * rotacionCamaraVertical) + (Vector3.down * rotacionCamaraHorizontal);
+
+
+            //rotacionCamaraVertical = 0f;
+            //rotacionCamaraHorizontal = 0f;
+            // Rota la cámara al rededor del eje X
+            personaje.Rotate(Vector3.right * inputX);
+        } else
+        {
+            
+        }
     }
 }
