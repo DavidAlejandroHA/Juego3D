b6390bab876111ee7e10d8df071cda2b1555a2b2
David Alejandro <99730787+DavidAlejandroHA@users.noreply.github.com>
2023-11-23 16:04:39 +0000

    Ejercicio 6 a medias

--- Assets/Scripts/RangoEnemigo/MovimientoEnemigoRaycast.cs
+++ Assets/Scripts/RangoEnemigo/MovimientoEnemigoRaycast.cs
@@ -7,7 +7,8 @@ public class MovimientoEnemigoRaycast : MonoBehaviour
     public int radio;
     public float velocidad = 3f;
     public GameObject target;
-    public string plaveholdef; 
+    public string plaveholdef;
+    bool seEncuentraConMuro = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,14 +28,53 @@ public class MovimientoEnemigoRaycast : MonoBehaviour
         if (listaChoques.Length > 0)
         {
             float step = velocidad * Time.deltaTime; // calcula la distancia a moverse
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+
+            RaycastHit choqueParedes;
+
+            if (Physics.Raycast(transform.position, transform.forward,
+                out choqueParedes, 0.75f))
+            {
+                if (choqueParedes.collider.gameObject != target)
+                {
+                    seEncuentraConMuro = true;
+                    //transform.RotateAround(transform.position, Vector3.right, 2f);
+                    Debug.Log("a");
+                    transform.Rotate(Vector3.right, 15);
+                    Invoke("ejemplo", 2f); // ejecuta una función con retraso, hacerlo para cambiar seEncuentraConMuro y rotar si hay un muro
+
+                    //transform.RotateAround(transform.position, Vector3.forward, 45f);
+                }
+                else
+                {
+
+                }
+            }
+
+            if (!seEncuentraConMuro)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
             transform.LookAt(target.transform.position, Vector3.left);
+            }
+            
+            
+
+            
         }
+
+        
+        //int mascaraParedes = 1 << 10;
+        //listaChoques = Physics.RaycastAll
+
+    }
+    void ejemplo()
+    {
+        Debug.Log("Hola!");
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color(255, 192, 203); // Color rosa
         Gizmos.DrawWireSphere(transform.position, radio);
+        Gizmos.DrawRay(transform.position, transform.forward);
     }
 }
